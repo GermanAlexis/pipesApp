27e3c0895bd366b5d4dd52eadcc03e84db43ddbe
German Alexis <alexisdager93@hotmail.com>
2023-12-04 22:37:36 -0500

    chore: added components into use

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+import { FieldsetModule } from 'primeng/fieldset';
 
 @NgModule({
-  exports: [MenubarModule],
+  exports: [
+    MenubarModule,
+    CardModule,
+    PanelModule,
+    ButtonModule,
+    FieldsetModule,
+  ],
 })
 export class PrimeNgModule {}
