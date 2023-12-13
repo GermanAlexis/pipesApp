fac66f42f91b8af1686709ae574e350a580e1585
German Alexis <alexisdager93@hotmail.com>
2023-12-13 07:56:15 -0500

    chore: pipes numerics

--- src/app/structure-pipe/pages/numeric-pipe-page/numeric-pipe-page.component.ts
+++ src/app/structure-pipe/pages/numeric-pipe-page/numeric-pipe-page.component.ts
@@ -5,4 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './numeric-pipe-page.component.html',
   styleUrls: ['./numeric-pipe-page.component.css'],
 })
-export class NumericPipePagesComponent {}
+export class NumericPipePagesComponent {
+  totalSell: number = 4565148654.2546;
+  percentSell: number = 0.3264;
+}
